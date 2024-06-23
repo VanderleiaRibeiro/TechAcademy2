2b29a8c6435de8b2df356de1878218cf85a27ba7
VanderleiaRibeiro <161781397+VanderleiaRibeiro@users.noreply.github.com>
2024-06-23 13:36:43 -0300

    criando footer responsivo

--- index.php
+++ index.php
@@ -107,14 +107,78 @@
 
 
   <footer>
-    <p style="text-align: leftt;"> Desenvolvido por: Vanderléia Ribeiro
-    </p>
-    <p style="text-align: right;">Redes sociais de Game Seven
-      <i class="bi bi-instagram"></i>
-      <i class="bi bi-facebook"></i>
-      <i class="bi bi-github"></i>
-      <i class="bi bi-linkedin"></i>
-    </p>
+    <div id="footer_content">
+      <div id="footer_contacts">
+        <h1>Game Seven</h1>
+        <p>Seu site de jogos.</p>
+
+        <div id="footer_social_media">
+          <a href="#" class="footer-link" id="instagram">
+            <i class="bi bi-instagram"></i>
+          </a>
+
+          <a href="#" class="footer-link" id="facebook">
+            <i class="bi bi-facebook""></i>
+                    </a>
+
+                    <a href=" #" class="footer-link" id="whatsapp">
+              <i class="bi bi-whatsapp"></i>
+          </a>
+        </div>
+      </div>
+
+      <ul class="footer-list">
+        <li>
+          <h3>Blog</h3>
+        </li>
+        <li>
+          <a href="#" class="footer-link">Tech</a>
+        </li>
+        <li>
+          <a href="#" class="footer-link">Adventures</a>
+        </li>
+        <li>
+          <a href="#" class="footer-link">Music</a>
+        </li>
+      </ul>
+
+      <ul class="footer-list">
+        <li>
+          <h3>Products</h3>
+        </li>
+        <li>
+          <a href="#" class="footer-link">App</a>
+        </li>
+        <li>
+          <a href="#" class="footer-link">Desktop</a>
+        </li>
+        <li>
+          <a href="#" class="footer-link">Cloud</a>
+        </li>
+      </ul>
+      <!-- 
+            <div id="footer_subscribe">
+                <h3>Subscribe</h3>
+
+                <p>
+                    Enter your e-mail to get notified about
+                    our news solutions
+                </p>
+
+                <div id="input_group">
+                    <input type="email" id="email">
+                    <button>
+                        <i class="fa-regular fa-envelope"></i>
+                    </button>
+                </div>
+            </div> -->
+    </div>
+
+    <div id="footer_copyright">
+      &#169
+      2024, Todos os direitos reservados
+      <br>Desenvolvido por: Vanderléia Ribeiro
+    </div>
   </footer>
 
   <!--scripts-->
